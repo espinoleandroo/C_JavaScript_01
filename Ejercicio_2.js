fb4b44076654e802f2473349660464dd1d517816
Leandro Espino <espinoleandroo@gmail.com>
2020-03-16 22:00:53 -0600

    Ejercicio 2

---
+++ Ejercicio_2.js
@@ -0,0 +1,15 @@
+//Recibir dos números usando prompt y sumarlos, restarlos, dividirlos y multiplicarlos
+
+let num1 = prompt();
+let num2 = prompt();
+
+let suma = parseInt(num1) + parseInt(num2);
+let resta = parseInt(num1) - parseInt(num2);
+let producto = parseInt(num1) * parseInt(num2);
+let divicion = parseInt(num1) / parseInt(num2);
+
+
+console.log("suma " + suma);
+console.log("resta " + resta);
+console.log("producto " + producto);
+console.log("divicion " + divicion);
